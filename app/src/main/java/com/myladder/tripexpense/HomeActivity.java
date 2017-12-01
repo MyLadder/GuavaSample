e9ce94286a4471d3715b408d08acb8b0a7ffdd6a
MyLadder <projects.android.manu@gmail.com>
2017-12-01 19:04:29 +0400

    initial commit

--- app/src/main/java/com/myladder/tripexpense/HomeActivity.java
+++ app/src/main/java/com/myladder/tripexpense/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.myladder.tripexpense;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,45 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //List of host data
+        List<Hosting> list = new ArrayList<>();
+        list.add(new Hosting(1, "liquidweb.com", 80000));
+        list.add(new Hosting(2, "linode.com", 90000));
+        list.add(new Hosting(3, "digitalocean.com", 120000));
+        list.add(new Hosting(4, "aws.amazon.com", 200000));
+        list.add(new Hosting(5, "mkyong.com", 1));
+
+//        // key = id, value - websites
+//        Map<Integer, String> result1 = list.stream().collect(
+//                Collectors.toMap(Hosting::getId, Hosting::getName));
+//
+//        System.out.println("Result 1 : " + result1);
+//
+//        // key = name, value - websites
+//        Map<String, Long> result2 = list.stream().collect(
+//                Collectors.toMap(Hosting::getName, Hosting::getWebsites));
+//
+//        System.out.println("Result 2 : " + result2);
+//
+//        // Same with result1, just different syntax
+//        // key = id, value = name
+//        Map<Integer, String> result3 = list.stream().collect(
+//                Collectors.toMap(x -> x.getId(), x -> x.getName()));
+//
+//        System.out.println("Result 3 : " + result3);
+
+
+
+
+        List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi",
+                "mandarin", "date", "quince");
+
+        for (String fruit: fruits) {
+            System.out.println(fruit);
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/myladder/tripexpense/Hosting.java
@@ -0,0 +1,43 @@
+package com.myladder.tripexpense;
+
+/**
+ * Created by Manu on 12/1/2017.
+ */
+
+public class Hosting {
+    private int Id;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getWebsites() {
+        return websites;
+    }
+
+    public void setWebsites(long websites) {
+        this.websites = websites;
+    }
+
+    private String name;
+    private long websites;
+
+    public Hosting(int id, String name, long websites) {
+        Id = id;
+        this.name = name;
+        this.websites = websites;
+    }
+
+}
