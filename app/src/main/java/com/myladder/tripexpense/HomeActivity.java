6ffa776aad1a389a30d073c541a04a3b9254efd2
Manu Aravind <34127320+MyLadder@users.noreply.github.com>
2017-12-01 19:08:42 +0400

    multimap

--- app/src/main/java/com/myladder/tripexpense/HomeActivity.java
+++ app/src/main/java/com/myladder/tripexpense/HomeActivity.java
@@ -9,5 +9,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+         Multimap<String, Integer> scores = HashMultimap.create();
+        scores.put("Bob", 20);
+        scores.put("Bob", 10);
+        scores.put("Bob", 15);
+        System.out.println(Collections.max(scores.get("Bob"))); // prints 20
     }
 }
